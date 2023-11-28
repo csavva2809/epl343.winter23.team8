6cf7ebb869696507ac402850f257f3fa33cb2576
Konstantinos <ksavva05@ucy.ac.cy>
2023-11-28 18:16:28 +0200

    added a registration.php file that interacts with the datebase and modified login.html to store/interact to/with the database

---
+++ registration.php
@@ -0,0 +1,20 @@
+<?php 
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $conn =  new mysqli('localhost','root','','test');
+
+    if($conn -> connect_error){
+        die('Connection Failed :' .$conn->connect_error);
+    }else {
+        $stmt = $conn->prepare("INSERT INTO userinfo(firstName, lastName, email, password) VALUES (?, ?, ?, ?)");
+        $stmt -> bind_param("ssss", $firstName, $lastName, $email, $password);
+        $stmt-> execute();
+        echo "Registration Successfully";
+        $stmt->close();
+        $conn->close();
+    }
+
+?>
