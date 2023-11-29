fb3be53dcae874696d7daee399a5702aee744ada
Konstantinos <ksavva05@ucy.ac.cy>
2023-11-29 17:14:26 +0200

    commit to sync with other members

--- login.php
+++ login.php
@@ -1,3 +1,24 @@
+<?php 
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $conn =  new mysqli("localhost","root","","epl343team8");
+
+    if($conn -> connect_error){
+        die('Connection Failed :' .$conn->connect_error);
+    }else {
+        $stmt = $conn->prepare("INSERT *INTO userinfo(firstName, lastName, email, password) VALUES ($firstName, $lastName, $email, $password)");
+        $stmt -> bind_param("ssss", $firstName, $lastName, $email, $password);
+        $stmt-> execute();
+        echo "Registration Successfully";
+        $stmt->close();
+        $conn->close();
+    }
+    
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
--- registration.php
+++ registration.php
@@ -4,17 +4,17 @@
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $conn =  new mysqli("localhost","root","","epl343.userinfo");
+    $conn =  new mysqli("localhost","root","","C:\\xampp\mysql\data\\epl343team8");
 
     if($conn -> connect_error){
         die('Connection Failed :' .$conn->connect_error);
     }else {
-        $stmt = $conn->prepare("INSERT *INTO userinfo(firstName, lastName, email, password) VALUES (?, ?, ?, ?)");
+        $stmt = $conn->prepare("INSERT *INTO userinfo(firstName, lastName, email, password) VALUES ($firstName, $lastName, $email, $password)");
         $stmt -> bind_param("ssss", $firstName, $lastName, $email, $password);
         $stmt-> execute();
         echo "Registration Successfully";
         $stmt->close();
         $conn->close();
     }
-
+    
 ?>
